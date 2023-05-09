2d6b4feaac8053577c7292429d8d81c6da93ca8c
Chinonso <Nonsodavid433@gmail.com>
2023-05-09 08:53:57 -0700

    if_else_statement

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        ascii_val = ord(c)
+        if ascii_val >= 97 and ascii_val <= 122:
+            ascii_val -= 32
+        print("{}".format(chr(ascii_val)), end="")
+    print("")
