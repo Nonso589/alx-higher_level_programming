84db1260f763c7a19c16ae85feb890146e2dad40
Chinonso <Nonsodavid433@gmail.com>
2023-05-30 00:48:25 -0700

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    empty_count = []
+    for j in range(list_length):
+        try:
+            re = my_list_1[j]/my_list_2[j]
+        except IndexError:
+            print("out of range")
+            re = 0
+        except TypeError:
+            print("wrong type")
+            re = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            re = 0
+        finally:
+            empty_count.append(re)
+    return empty_count
